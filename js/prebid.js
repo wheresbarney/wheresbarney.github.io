2b3028df423902287797437d1e5823282de011e2
Barney Moss <barn@wheresbarney.com>
2017-05-17 11:57:00 +0100

    My fixes to Prebid master

--- js/prebid.js
+++ js/prebid.js
@@ -5698,8 +5698,6 @@
 
 	'use strict';
 	
-	var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
-	
 	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 	
 	var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; /**
@@ -5737,19 +5735,35 @@
 	};
 	
 	/**
-	 * Return a copy of a bid with slot sizes flattened and filtered
-	 * @param {Object} bid
-	 * @returns {Object} copy of bid
+	 * Get a custom FB format if so configured
 	 */
-	var flattenBidRequestSizes = function flattenBidRequestSizes(bid) {
-	  var sizes = Array.isArray(bid.sizes) && bid.sizes.map(flattenSize).filter(isValidSize);
+	var getFBFormat = function getFBFormat(bid) {
+	  return bid.params.fullwidth ? 'fullwidth' : bid.params.native ? 'native' : null;
+	};
+	
+	/**
+	 * Return a copy of a bid with slot sizes filtered
+	 * @param {Object} bid
+	 * @returns {Object} Copy of bid
+	 */
+	var filterBidRequestSizes = function filterBidRequestSizes(bid) {
+	  var sizes = Array.isArray(bid.sizes) && bid.sizes.filter(isValidSize).slice(0, 1);
 	  return _extends({}, bid, { sizes: sizes });
+	};
+	
+	/**
+	 * Return a flattened size, or a format string
+	 * @param {Object} bid
+	 * @returns {String} size or format
+	 */
+	var flattenBidRequestSize = function flattenBidRequestSize(bid, size) {
+	  return getFBFormat(bid) || flattenSize(size);
 	};
 	
 	/**
 	 * Flattens a 2-element [W, H] array as a 'WxH' string,
 	 * otherwise passes value through.
-	 * @params {Array|String} size
+	 * @param {Array|String} size
 	 * @returns {String}
 	 */
 	var flattenSize = function flattenSize(size) {
@@ -5758,11 +5772,15 @@
 	
 	/**
 	 * Is this a valid slot size?
-	 * @param {String} size
-	 * @returns  {Boolean}
+	 * @param {Array} size
+	 * @returns {Boolean}
 	 */
 	var isValidSize = function isValidSize(size) {
-	  return ['native', 'fullwidth', '300x250', '320x50'].includes(size);
+	  return Array.isArray(size) && size.length === 2 && [[300, 250], [320, 50]].some(function (good) {
+	    return good.every(function (val, i) {
+	      return val === size[i];
+	    });
+	  });
 	};
 	
 	/**
@@ -5788,25 +5806,16 @@
 	};
 	
 	/**
-	 * Is this a native advert size?
-	 * @param {String} size
-	 * @returns {Boolean}
-	 */
-	var isNative = function isNative(size) {
-	  return ['native', 'fullwidth'].includes(size);
-	};
-	
-	/**
 	 * Generate ad HTML for injection into an iframe
 	 * @param {String} placementId
 	 * @param {String} size
 	 * @param {String} bidId
 	 * @returns {String} HTML
 	 */
-	var createAdHtml = function createAdHtml(placementId, size, bidId) {
-	  var nativeStyle = isNative(size) ? '<script>window.onload=function(){if(parent){var o=document.getElementsByTagName("head")[0];var s=parent.document.getElementsByTagName("style");for(var i=0;i<s.length;i++)o.appendChild(s[i].cloneNode(true));}}</script>' : '';
-	  var nativeContainer = isNative(size) ? '<div class="thirdPartyRoot"><a class="fbAdLink"><div class="fbAdMedia thirdPartyMediaClass"></div><div class="fbAdSubtitle thirdPartySubtitleClass"></div><div class="fbDefaultNativeAdWrapper"><div class="fbAdCallToAction thirdPartyCallToActionClass"></div><div class="fbAdTitle thirdPartyTitleClass"></div></div></a></div>' : '';
-	  return '<html><head>' + nativeStyle + '</head><body><div style="display:none;position:relative;">\n<script>var data = {placementid:\'' + placementId + '\',format:\'' + size + '\',bidid:\'' + bidId + '\',onAdLoaded:function(e){e.style.display = \'block\';},onAdError:function(c,m){console.log(\'Audience Network error (\' + c + \') \' + m);}};\n(function(a,b,c){var d=\'https://www.facebook.com\',e=\'https://connect.facebook.net/en_US/fbadnw55.js\',f={iframeLoaded:true,xhrLoaded:true},g=5,h=a.data,i=0,j=function(ea){if(ea==null)throw new Error();return ea;},k=function(ea){if(ea instanceof HTMLElement)return ea;throw new Error();},l=function(){if(Date.now){return Date.now();}else return +new Date();},m=function(ea){if(++i>g)return;var fa=d+\'/audience_network/client_event\',ga={cb:l(),event_name:\'ADNW_ADERROR\',ad_pivot_type:\'audience_network_mobile_web\',sdk_version:\'5.5.web\',app_id:h.placementid.split(\'_\')[0],publisher_id:h.placementid.split(\'_\')[1],error_message:ea},ha=[];for(var ia in ga)ha.push(encodeURIComponent(ia)+\'=\'+encodeURIComponent(ga[ia]));var ja=fa+\'?\'+ha.join(\'&\'),ka=new XMLHttpRequest();ka.open(\'GET\',ja,true);ka.send();},n=function(){if(b.currentScript){return b.currentScript;}else{var ea=b.getElementsByTagName(\'script\');return ea[ea.length-1];}},o=function(ea){try{return ea.document.referrer;}catch(fa){}return \'\';},p=function(){var ea=a;try{while(ea!=ea.parent){ea.parent.origin;ea=ea.parent;}}catch(fa){}return ea;},q=function(ea){var fa=ea.indexOf(\'/\',ea.indexOf(\'://\')+3);if(fa===-1)return ea;return ea.substring(0,fa);},r=function(ea){return ea.location.href||o(ea);},s=function(ea,fa){if(ea.sdkLoaded)return;var ga=fa.createElement(\'iframe\');ga.name=\'fbadnw\';ga.style.display=\'none\';j(fa.body).appendChild(ga);ga.contentWindow.addEventListener(\'error\',function(event){m(event.message);},false);var ha=ga.contentDocument.createElement(\'script\');ha.src=e;ha.async=true;j(ga.contentDocument.body).appendChild(ha);ea.sdkLoaded=true;},t=function(ea){var fa=/^https?:\\/\\/www\\.google(\\.com?)?.\\w{2,3}$/;return !!ea.match(fa);},u=function(ea){return ea.endsWith(\'cdn.ampproject.org\');},v=function(){var ea=c.ancestorOrigins||[],fa=ea[ea.length-1]||c.origin,ga=ea[ea.length-2]||c.origin;if(t(fa)&&u(ga)){return q(ga);}else return q(fa);},w=function(ea){try{return JSON.parse(ea);}catch(fa){m(fa.message);return null;}},x=function(ea,fa,ga){if(!ea.iframe){var ha=ga.createElement(\'iframe\');ha.src=d+\'/audiencenetwork/iframe/\';ha.style.display=\'none\';j(ga.body).appendChild(ha);ea.iframe=ha;ea.iframeAppendedTime=l();ea.iframeData={};}fa.iframe=j(ea.iframe);fa.iframeData=ea.iframeData;fa.tagJsIframeAppendedTime=ea.iframeAppendedTime||0;},y=function(ea){var fa=d+\'/audiencenetwork/xhr/?sdk=5.5.web\';for(var ga in ea)if(typeof ea[ga]!==\'function\')fa+=\'&\'+ga+\'=\'+encodeURIComponent(ea[ga]);var ha=new XMLHttpRequest();ha.open(\'GET\',fa,true);ha.withCredentials=true;ha.onreadystatechange=function(){if(ha.readyState===4){var ia=w(ha.response);if(ia)ea.events.push({name:\'xhrLoaded\',source:ea.iframe.contentWindow,data:ia,postMessageTimestamp:l(),receivedTimestamp:l()});}};ha.send();},z=function(ea,fa){var ga=d+\'/audiencenetwork/xhriframe/?sdk=5.5.web\';for(var ha in fa)if(typeof fa[ha]!==\'function\')ga+=\'&\'+ha+\'=\'+encodeURIComponent(fa[ha]);var ia=b.createElement(\'iframe\');ia.src=ga;ia.style.display=\'none\';j(b.body).appendChild(ia);fa.iframe=ia;fa.iframeData={};fa.tagJsIframeAppendedTime=l();},aa=function(ea){var fa=function(event){try{var ia=event.data;if(ia.name in f)ea.events.push({name:ia.name,source:event.source,data:ia.data});}catch(ha){}},ga=j(ea.iframe).contentWindow.parent;ga.addEventListener(\'message\',fa,false);},ba=function(ea){if(ea.context)return true;try{return !!JSON.parse(decodeURI(ea.name)).ampcontextVersion;}catch(fa){return false;}},ca=function(ea){var fa=l(),ga=p(),ha=k(n().parentElement),ia=ga!=a.top,ja=ga.$sf&&ga.$sf.ext,ka=r(ga);ga.ADNW=ga.ADNW||{};ga.ADNW.v55=ga.ADNW.v55||{ads:[]};var la=ga.ADNW.v55;s(la,ga.document);var ma={amp:ba(ga),events:[],tagJsInitTime:fa,rootElement:ha,iframe:null,tagJsIframeAppendedTime:la.iframeAppendedTime||0,url:ka,domain:v(),channel:q(r(ga)),width:screen.width,height:screen.height,pixelratio:a.devicePixelRatio,placementindex:la.ads.length,crossdomain:ia,safeframe:!!ja,placementid:h.placementid,format:h.format||\'300x250\',testmode:!!h.testmode,onAdLoaded:h.onAdLoaded,onAdError:h.onAdError};if(h.bidid)ma.bidid=h.bidid;if(ia){z(la,ma);}else{x(la,ma,ga.document);y(ma);}aa(ma);ma.rootElement.dataset.placementid=ma.placementid;la.ads.push(ma);};try{ca();}catch(da){m(da.message||da);throw da;}})(window,document,location);\n</script>\n' + nativeContainer + '</div></body></html>';
+	var createAdHtml = function createAdHtml(placementId, format, bidId) {
+	  var nativeStyle = format === 'native' ? '<script>window.onload=function(){if(parent){var o=document.getElementsByTagName("head")[0];var s=parent.document.getElementsByTagName("style");for(var i=0;i<s.length;i++)o.appendChild(s[i].cloneNode(true));}}</script>' : '';
+	  var nativeContainer = format === 'native' ? '<div class="thirdPartyRoot"><a class="fbAdLink"><div class="fbAdMedia thirdPartyMediaClass"></div><div class="fbAdSubtitle thirdPartySubtitleClass"></div><div class="fbDefaultNativeAdWrapper"><div class="fbAdCallToAction thirdPartyCallToActionClass"></div><div class="fbAdTitle thirdPartyTitleClass"></div></div></a></div>' : '';
+	  return '<html><head>' + nativeStyle + '</head><body><div style="display:none;position:relative;">\n<script type=\'text/javascript\'>var data = {placementid:\'' + placementId + '\',format:\'' + format + '\',bidid:\'' + bidId + '\',onAdLoaded:function(e){console.log(\'Audience Network [' + placementId + '] ad loaded\');e.style.display = \'block\';},onAdError:function(c,m){console.log(\'Audience Network [' + placementId + '] error (\' + c + \') \' + m);}};\n(function(a,b,c){var d=\'https://www.facebook.com\',e=\'https://connect.facebook.net/en_US/fbadnw55.js\',f={iframeLoaded:true,xhrLoaded:true},g=a.data,h=function(){if(Date.now){return Date.now();}else return +new Date();},i=function(aa){var ba=d+\'/audience_network/client_event\',ca={cb:h(),event_name:\'ADNW_ADERROR\',ad_pivot_type:\'audience_network_mobile_web\',sdk_version:\'5.5.web\',app_id:g.placementid.split(\'_\')[0],publisher_id:g.placementid.split(\'_\')[1],error_message:aa},da=[];for(var ea in ca)da.push(encodeURIComponent(ea)+\'=\'+encodeURIComponent(ca[ea]));var fa=ba+\'?\'+da.join(\'&\'),ga=new XMLHttpRequest();ga.open(\'GET\',fa,true);ga.send();if(g.onAdError)g.onAdError(\'1000\',\'Internal error.\');},j=function(){if(b.currentScript){return b.currentScript;}else{var aa=b.getElementsByTagName(\'script\');return aa[aa.length-1];}},k=function(aa){try{return aa.document.referrer;}catch(ba){}return \'\';},l=function(){var aa=a,ba=[aa];try{while(aa!==aa.parent&&aa.parent.document)ba.push(aa=aa.parent);}catch(ca){}return ba.reverse();},m=function(){var aa=l();for(var ba=0;ba<aa.length;ba++){var ca=aa[ba],da=ca.ADNW||{};ca.ADNW=da;if(!ca.ADNW)continue;return da.v55=da.v55||{ads:[],window:ca};}throw new Error(\'no_writable_global\');},n=function(aa){var ba=aa.indexOf(\'/\',aa.indexOf(\'://\')+3);if(ba===-1)return aa;return aa.substring(0,ba);},o=function(aa){return aa.location.href||k(aa);},p=function(aa){if(aa.sdkLoaded)return;var ba=aa.window.document,ca=ba.createElement(\'iframe\');ca.name=\'fbadnw\';ca.style.display=\'none\';ba.body.appendChild(ca);var da=ca.contentDocument.createElement(\'script\');da.src=e;da.async=true;ca.contentDocument.body.appendChild(da);aa.sdkLoaded=true;},q=function(aa){var ba=/https?:\\/\\/www\\.google(\\.com?)?\\.\\w{2,3}$/;return !!aa.match(ba);},r=function(aa){return !!aa.match(/cdn\\.ampproject\\.org$/);},s=function(){var aa=c.ancestorOrigins||[],ba=aa[aa.length-1]||c.origin,ca=aa[aa.length-2]||c.origin;if(q(ba)&&r(ca)){return n(ca);}else return n(ba);},t=function(aa){try{return JSON.parse(aa);}catch(ba){i(ba.message);throw ba;}},u=function(aa,ba,ca){if(!aa.iframe){var da=ca.createElement(\'iframe\');da.src=d+\'/audiencenetwork/iframe/\';da.style.display=\'none\';ca.body.appendChild(da);aa.iframe=da;aa.iframeAppendedTime=h();aa.iframeData={};}ba.iframe=aa.iframe;ba.iframeData=aa.iframeData;ba.tagJsIframeAppendedTime=aa.iframeAppendedTime||0;},v=function(aa){var ba=d+\'/audiencenetwork/xhr/?sdk=5.5.web\';for(var ca in aa)if(typeof aa[ca]!==\'function\')ba+=\'&\'+ca+\'=\'+encodeURIComponent(aa[ca]);var da=new XMLHttpRequest();da.open(\'GET\',ba,true);da.withCredentials=true;da.onreadystatechange=function(){if(da.readyState===4){var ea=t(da.response);aa.events.push({name:\'xhrLoaded\',source:aa.iframe.contentWindow,data:ea,postMessageTimestamp:h(),receivedTimestamp:h()});}};da.send();},w=function(aa,ba){var ca=d+\'/audiencenetwork/xhriframe/?sdk=5.5.web\';for(var da in ba)if(typeof ba[da]!==\'function\')ca+=\'&\'+da+\'=\'+encodeURIComponent(ba[da]);var ea=b.createElement(\'iframe\');ea.src=ca;ea.style.display=\'none\';b.body.appendChild(ea);ba.iframe=ea;ba.iframeData={};ba.tagJsIframeAppendedTime=h();},x=function(aa){var ba=function(event){try{var da=event.data;if(da.name in f)aa.events.push({name:da.name,source:event.source,data:da.data});}catch(ea){}},ca=aa.iframe.contentWindow.parent;ca.addEventListener(\'message\',ba,false);},y=function(aa){if(aa.context&&aa.context.sourceUrl)return true;try{return !!JSON.parse(decodeURI(aa.name)).ampcontextVersion;}catch(ba){return false;}},z=function(aa){var ba=h(),ca=l()[0],da=j().parentElement,ea=ca!=a.top,fa=ca.$sf&&ca.$sf.ext,ga=o(ca),ha=m();p(ha);var ia={amp:y(ca),events:[],tagJsInitTime:ba,rootElement:da,iframe:null,tagJsIframeAppendedTime:ha.iframeAppendedTime||0,url:ga,domain:s(),channel:n(o(ca)),width:screen.width,height:screen.height,pixelratio:a.devicePixelRatio,placementindex:ha.ads.length,crossdomain:ea,safeframe:!!fa,placementid:g.placementid,format:g.format||\'300x250\',testmode:!!g.testmode,onAdLoaded:g.onAdLoaded,onAdError:g.onAdError};if(g.bidid)ia.bidid=g.bidid;if(ea){w(ha,ia);}else{u(ha,ia,ca.document);v(ia);}; x(ia);ia.rootElement.dataset.placementid=ia.placementid;ha.ads.push(ia);};try{z();}catch(aa){i(aa.message||aa);throw aa;}})(window,document,location);\n</script>\n' + nativeContainer + '</div></body></html>';
 	};
 	
 	/**
@@ -5817,24 +5826,21 @@
 	 * @param {Number} cpmCents
 	 * @returns {Object} Bid
 	 */
-	var createSuccessBidResponse = function createSuccessBidResponse(placementId, size, bidId, cpmCents) {
+	var createSuccessBidResponse = function createSuccessBidResponse(placementId, size, bidId, cpmCents, format) {
 	  var bid = (0, _bidfactory.createBid)(_constants.STATUS.GOOD, { bidId: bidId });
 	  // Prebid attributes
 	  bid.bidderCode = getBidderCode();
 	  bid.cpm = cpmCents / 100;
-	  bid.ad = createAdHtml(placementId, size, bidId);
-	  if (!isNative(size)) {
-	    var _size$split$map = size.split('x').map(Number);
+	  bid.ad = createAdHtml(placementId, format, bidId);
 	
-	    var _size$split$map2 = _slicedToArray(_size$split$map, 2);
 	
-	    bid.width = _size$split$map2[0];
-	    bid.height = _size$split$map2[1];
-	  }
 	  // Audience Network attributes
+	  var _ref = [size[0], size[1]];
+	  bid.width = _ref[0];
+	  bid.height = _ref[1];
 	  bid.hb_bidder = 'fan';
 	  bid.fb_bidid = bidId;
-	  bid.fb_format = size;
+	  bid.fb_format = format;
 	  bid.fb_placementid = placementId;
 	  return bid;
 	};
@@ -5856,8 +5862,10 @@
 	 * @param {String} params.bids[].placementCode - Prebid placement identifier
 	 * @param {Object} params.bids[].params
 	 * @param {String} params.bids[].params.placementId - Audience Network placement identifier
-	 * @param {Array} params.bids[].sizes - list of accepted advert sizes
-	 * @param {Array|String} params.bids[].sizes[] - one of 'native', '300x250', '300x50', [300, 250], [300, 50]
+	 * @param {Boolean} params.bids[].params.fullwidth - fullwidth flag or unset
+	 * @param {Boolean} params.bids[].params.native - native flag or unset
+	 * @param {Array} params.bids[].sizes - list of desired advert sizes
+	 * @param {Array} params.bids[].sizes[] - Size arrays [h,w]: should include one of [300, 250], [320, 50]: first matched size is used
 	 * @returns {void}
 	 */
 	var callBids = function callBids(bidRequest) {
@@ -5865,12 +5873,12 @@
 	  var adUnitCodes = [];
 	  var placementids = [];
 	  var adformats = [];
-	  bidRequest.bids.map(flattenBidRequestSizes).filter(validateBidRequest).forEach(function (bid) {
-	    return bid.sizes.forEach(function (size) {
-	      adUnitCodes.push(bid.placementCode);
-	      placementids.push(bid.params.placementId);
-	      adformats.push(size);
-	    });
+	  var sizes = [];
+	  bidRequest.bids.map(filterBidRequestSizes).filter(validateBidRequest).forEach(function (bid) {
+	    adUnitCodes.push(bid.placementCode);
+	    placementids.push(bid.params.placementId);
+	    adformats.push(flattenBidRequestSize(bid, bid.sizes[0]));
+	    sizes.push(bid.sizes[0]);
 	  });
 	
 	  if (placementids.length) {
@@ -5910,7 +5918,7 @@
 	        }, [])
 	        // call addBidResponse
 	        .forEach(function (bid, i) {
-	          return (0, _bidmanager.addBidResponse)(adUnitCodes[i], createSuccessBidResponse(bid.placement_id, adformats[i], bid.bid_id, bid.bid_price_cents));
+	          return (0, _bidmanager.addBidResponse)(adUnitCodes[i], createSuccessBidResponse(bid.placement_id, sizes[i], bid.bid_id, bid.bid_price_cents, adformats[i]));
 	        });
 	      }
 	    }, null, { withCredentials: true });
