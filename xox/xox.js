3c3996223c07e4ff4e1baf3c0c7b481dbb2f3c8b
Barney Moss <barn@wheresbarney.com>
2022-01-29 19:11:04 +0000

    More stateful version

--- xox/xox.js
+++ xox/xox.js
@@ -1,8 +1,15 @@
-BoardStateEnum = {
-  OPEN: "open",
-  CLOSED: "closed",
+// 'use strict';
+
+/*
+Bugs
+1. draw CSS, closed+won CSS
+2. Fix win generator (click on correct board!)
+*/
+
+WinState = {
   X_WON: "x_won",
-  O_WON: "o_won"
+  O_WON: "o_won",
+  DRAW: "draw"
 };
 
 function Square(props) {
@@ -35,61 +42,87 @@ class RegularBoard extends React.Component {
     for (let cell = 0; cell < 9; ++cell) {
       board.push(this.renderSquare(cell));
     }
-    return <div className={"inner " + this.props.state}>{board}</div>;
+    return (
+      <div
+        className={
+          "inner " +
+          this.props.winState +
+          " " +
+          (this.props.playable ? "playable" : "closed")
+        }
+      >
+        {board}
+      </div>
+    );
   }
 }
 
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    this.reset();
-  }
-
-  reset() {
-    const squares = [];
-    for (let i = 0; i < 9; ++i) {
-      squares.push(Array(9).fill(null));
-    }
     this.state = {
-      squares: squares,
-      lastClicked: {
-        board: null,
-        index: 0
-      },
+      // outer array: boards; inner arrays: cells in board
+      squares: Array.from(Array(9), () => Array(9).fill(null)),
+      wins: Array(9).fill(null),
+      playable: [true].concat(Array(8).fill(false)),
       xIsNext: true
     };
   }
 
-  testWin(who) {
-    const squares = [];
-    for (let i = 0; i < 9; ++i) {
-      squares.push(Array(9).fill(null));
-    }
-    squares[0] = Array(9).fill(who);
-    squares[1] = Array(9).fill(who);
-    squares[2] = Array(9).fill(who);
-
+  reset() {
     this.setState({
-      squares: squares,
-      lastClicked: {
-        board: 2,
-        index: 3
-      },
+      // outer array: boards; inner arrays: cells in board
+      squares: Array.from(Array(9), () => Array(9).fill(null)),
+      wins: Array(9).fill(null),
+      playable: [true].concat(Array(8).fill(false)),
       xIsNext: true
     });
   }
 
-  calculateBoardWinner(board) {
-    const winner = this.calculateWinnerImpl(this.state.squares[board]);
-    if (winner)
-      return winner === "X" ? BoardStateEnum.X_WON : BoardStateEnum.O_WON;
+  generateTestWin(winner) {
+    const loser = winner === "X" ? "O" : "X";
+    const wonBoardState = [
+      winner,
+      loser,
+      null,
+      loser,
+      winner,
+      null,
+      loser,
+      null,
+      winner
+    ];
+    const winnerState = winner === "X" ? WinState.X_WON : WinState.O_WON;
+
+    this.setState({
+      squares: [
+        wonBoardState.slice(),
+        wonBoardState.slice(),
+        wonBoardState.slice(0, 8, null).concat(null)
+      ].concat(Array.from(Array(6), () => Array(9).fill(null))),
+      wins: [winnerState, winnerState].concat(Array(7).fill(null)),
+      playable: [false, false, true, false, false, false, false, false, false],
+      xIsNext: winner === "X",
+      lastClicked: {
+        board: 0,
+        index: 2
+      }
+    });
+  }
+
+  calculateBoardWinner(cells) {
+    const winner = this.calculateWinnerImpl(cells);
+    if (winner) {
+      return winner === "X" ? WinState.X_WON : WinState.O_WON;
+    }
+    if (cells.every((x) => x)) {
+      return WinState.DRAW;
+    }
     return null;
   }
 
   calculateOverallWinner() {
-    return this.calculateWinnerImpl(
-      this.state.squares.map((sq) => this.calculateWinnerImpl(sq))
-    );
+    return this.calculateWinnerImpl(this.state.wins);
   }
 
   calculateWinnerImpl(squares) {
@@ -118,43 +151,47 @@ class Game extends React.Component {
   }
 
   handleClick(board, index) {
-    if (board !== this.state.lastClicked.index) {
-      return;
-    }
-    if (this.calculateBoardWinner(board) || this.state.squares[board][index]) {
+    if (!this.state.playable[board] || this.state.squares[board][index]) {
       return;
     }
 
     const squares = this.state.squares.slice();
     squares[board][index] = this.state.xIsNext ? "X" : "O";
-    this.setState({
+
+    const wins = this.state.wins.slice();
+    wins[board] = this.calculateBoardWinner(squares[board]);
+    const nextIsComplete = squares[index].every((x) => x);
+    const playable = squares.map((sq, i) => {
+      if (nextIsComplete) {
+        // every incomplete square is open
+        return !sq.every((x) => x);
+      }
+      return i === index;
+    });
+
+    this.setState((state) => ({
       squares: squares,
+      wins: wins,
+      playable: playable,
       lastClicked: {
         board: board,
         index: index
       },
-      xIsNext: !this.state.xIsNext
-    });
+      xIsNext: !state.xIsNext
+    }));
   }
 
   render() {
     const winner = this.calculateOverallWinner();
     let status;
     if (winner) {
-      status = "Winner: " + winner;
+      status = "Winner: " + winner === WinState.X_WON ? "X" : "O";
     } else {
       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
 
     const boards = [];
     for (let b = 0; b < 9; ++b) {
-      const winner = this.calculateBoardWinner(b);
-      const state = winner
-        ? winner
-        : this.state.lastClicked?.index === b
-        ? BoardStateEnum.OPEN
-        : BoardStateEnum.CLOSED;
-
       boards.push(
         <RegularBoard
           key={b}
@@ -164,7 +201,8 @@ class Game extends React.Component {
               ? this.state.lastClicked.index
               : null
           }
-          state={state}
+          winState={this.state.wins[b]}
+          playable={this.state.playable[b]}
           onClick={(i) => this.handleClick(b, i)}
         />
       );
@@ -175,8 +213,8 @@ class Game extends React.Component {
         <div className="game-info">
           <div>{status}</div>
           <button onClick={() => this.reset()}>Reset</button>
-          <button onClick={() => this.testWin("X")}>X wins</button>
-          <button onClick={() => this.testWin("Y")}>Y wins</button>
+          <button onClick={() => this.generateTestWin("X")}>X wins</button>
+          <button onClick={() => this.generateTestWin("O")}>O wins</button>
         </div>
       </div>
     );
